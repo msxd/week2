8f28098643654219d304a7728331723a31652717
Alex <valikov.ids@gmail.com>
2014-04-23 15:35:04 +0300

    Меню в верхней части

---
+++ protected/views/layouts/menu.php
@@ -0,0 +1,12 @@
+<?
+$this->widget('zii.widgets.CMenu',array(
+	'items'=>array(
+		array('label'=>'Home', 'url'=>array('/site/index')),
+		array('label'=>'Sign in', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+		array('label'=>'Sign up', 'url'=>array('/site/registration'), 'visible'=>Yii::app()->user->isGuest),
+		array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+	),
+	'htmlOptions'=>array('id'=>'top')
+));
+
+?>
