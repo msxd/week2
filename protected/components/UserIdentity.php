7b5187ee776385536bf55266431fdafec64839ca
Alex <valikov.ids@gmail.com>
2014-04-24 16:47:53 +0300

    authentificate is work

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -4,6 +4,7 @@
  * UserIdentity represents the data needed to identity a user.
  * It contains the authentication method that checks if the provided
  * data can identity the user.
+ * @property User $user
  */
 class UserIdentity extends CUserIdentity
 {
@@ -15,19 +16,29 @@ class UserIdentity extends CUserIdentity
 	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
 	 */
+	private $_id;
+
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
+		/** @var User $record */
+		$record = User::model()->findByAttributes(array('email' => $this->username));
+
+		if (!$record)
+			$this->errorCode = self::ERROR_USERNAME_INVALID;
+		else if ($record->hashed_password != $this->password)
+			$this->errorCode = self::ERROR_PASSWORD_INVALID;
+		else {
+			$this->_id = $record->id;
+			$this->setState('facebook_id', $record->facebook_id);
+			$this->errorCode = self::ERROR_NONE;
+		}
+
+
 		return !$this->errorCode;
+	}
+
+	public function getId()
+	{
+		return $this->_id;
 	}
 }
