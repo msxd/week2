6386d8f90149158a117387bccc6def505406c728
Alex <valikov.ids@gmail.com>
2014-04-24 16:49:28 +0300

    Исправлен логин, в индекс добавлен не обяхательный параметр для запроса конкретного поста

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -20,18 +20,19 @@ class SiteController extends Controller
 	 * This is the default 'index' action that is invoked
 	 * when an action is not explicitly requested by users.
 	 */
-	public function actionIndex()
+	public function actionIndex($pid = '-')
 	{
 // renders the view file 'protected/views/site/index.php'
 // using the default layout 'protected/views/layouts/main.php'
-		$pid = isset($_GET['pid']) ? $_GET['pid'] : '-';
+		//$pid = isset($_GET['pid']) ? $_GET['pid'] : '-';
 		if ($pid === '-') {
 			//	$all_posts = Post::model()->hotNews()->with('user')->findAll();
 			$all_posts = Post::model()->published()->with('user')->findAll();
 			$this->render('index', array('posts' => $all_posts));
 		} else {
+			/** @var Post[] $post_with_pid */
 			$post_with_pid = Post::model()->getNew($pid)->with('comments')->with('user')->findAll();
-			if ($post_with_pid[0]['published'] != 0) {
+			if ($post_with_pid[0]->published != 0) {
 				$this->render('index', array('posts' => $post_with_pid));
 			} else {
 				//$this->redirect(Yii::app()->user->returnUrl);
@@ -62,32 +63,6 @@ class SiteController extends Controller
 		$this->render('index');
 	}
 
-	/**
-	 * Displays the login page
-	 *
-	 * public function actionLogin()
-	 * {
-	 * $model=new LoginForm;
-	 *
-	 * // if it is ajax validation request
-	 * if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-	 * {
-	 * echo CActiveForm::validate($model);
-	 * Yii::app()->end();
-	 * }
-	 *
-	 * // collect user input data
-	 * if(isset($_POST['LoginForm']))
-	 * {
-	 * $model->attributes=$_POST['LoginForm'];
-	 * // validate user input and redirect to the previous page if valid
-	 * if($model->validate() && $model->login())
-	 * $this->redirect(Yii::app()->user->returnUrl);
-	 * }
-	 * // display the login form
-	 * $this->render('login',array('model'=>$model));
-	 * }*/
-
 	public function actionLogin()
 	{
 		$model = new User('login');
@@ -101,9 +76,9 @@ class SiteController extends Controller
 		}
 		*/
 
-		if (isset($_POST['User'])) {
-			$model->attributes = $_POST['User'];
-			if ($model->validate()) {
+			if ($model->attributes = Yii::app()->request->getPost('User')) {
+
+			if ($model->login()) {
 				// form inputs are valid, do something here
 				$this->redirect(Yii::app()->user->returnUrl);
 				return;
