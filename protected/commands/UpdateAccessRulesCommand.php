39c9863b7a031bdf1b516f245ef835419f72434a
Alex <valikov.ids@gmail.com>
2014-04-25 16:50:01 +0300

    Команды для создания ролей

--- protected/commands/UpdateAccessRulesCommand.php
+++ protected/commands/UpdateAccessRulesCommand.php
@@ -8,34 +8,33 @@ class UpdateAccessRulesCommand extends CConsoleCommand
 
 		$auth->createOperation('editComment');
 		$auth->createOperation('editPost');
-		$auth->createOperation('editPage');
-
-		$auth->createOperation('complainComment', '', 'return Yii::app()->user->id != $params["model"]->user_id;');
-		$auth->createOperation('complainLook', '', 'return Yii::app()->user->id != $params["model"]->user_id;');
-		$auth->createOperation('complainUser', '', 'return true;');
+		$auth->createOperation('editUser');
+		$auth->createOperation('editUserStatus');
 
 		$auth->createTask('editOwnComment', '', 'return Yii::app()->user->id == $params["model"]->user_id;')
 			->addChild('editComment');
 
-		$auth->createTask('editOwnLook', '', 'return Yii::app()->user->id == $params["model"]->user_id;')
-			->addChild('editLook');
+		$auth->createTask('editOwnPost', '', 'return Yii::app()->user->id == $params["model"]->user_id;')
+			->addChild('editPost');
+
+		$auth->createTask('editOwnUser','','return Yii::app()->user->id == $params["$model"]->user_id')
+			->addChild('editUser');
 
 		$role = $auth->createRole(User::ROLE_USER);
 		$role->addChild('editOwnComment');
-		$role->addChild('editOwnLook');
-		$role->addChild('complainComment');
-		$role->addChild('complainLook');
-		$role->addChild('complainUser');
+		$role->addChild('editOwnPost');
+		$role->addChild('editOwnUser');
 
 		$role =$auth->createRole(User::ROLE_MODERATOR);
 		$role->addChild(User::ROLE_USER);
 		$role->addChild('editComment');
-		$role->addChild('editLook');
+		$role->addChild('editPost');
+		$role->addChild('editUserStatus');
 
 		$role=$auth->createRole(User::ROLE_ADMIN);
 		$role->addChild(User::ROLE_MODERATOR);
-		$role->addChild('editBanner');
-		$role->addChild('editPage');
+		$role->addChild('editUser');
+
 
 		$auth->save();
 	}
