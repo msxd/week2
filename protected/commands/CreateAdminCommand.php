7cc2368921f0c0334c849a18398a578622afa85e
Alex <valikov.ids@gmail.com>
2014-04-28 17:04:56 +0300

    Консольная комманда для создания суперпользователся

---
+++ protected/commands/CreateAdminCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+class CreateAdminCommand extends CConsoleCommand
+{
+	public function actionIndex($email='admin@adm.in', $first_name='admin', $last_name='admin', $phone='1230011223', $password='admin', $repeat_password='admin')
+	{
+		$model = new User();
+		$model->role_id = User::ROLE_ADMIN;
+		$model->email = $email;
+		$model->first_name = $first_name;
+		$model->last_name = $last_name;
+		$model->pass = $password;
+		$model->r_pass = $repeat_password;
+		$model->phone = $phone;
+		$model->approved = '1';
+
+		if ($model->save())
+			die('Success!');
+	}
+}
+
+
