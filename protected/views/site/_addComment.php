3951c3902ef62e02d2177e6691f587f30cb436f9
Alex <valikov.ids@gmail.com>
2014-05-19 17:01:11 +0300

    WYSIWYG для добавления комментариев

--- protected/views/site/_addComment.php
+++ protected/views/site/_addComment.php
@@ -7,38 +7,45 @@
 
 <div class="form">
 
-	<?php $form=$this->beginWidget('CActiveForm', array(
-		'id'=>'comment-_addComment-form',
-		// Please note: When you enable ajax validation, make sure the corresponding
-		// controller action is handling ajax validation correctly.
-		// See class documentation of CActiveForm for details on this,
-		// you need to use the performAjaxValidation()-method described there.
+	<?php  $form=$this->beginWidget('CActiveForm', array(
+		'id'=>'users-signin-form',
+		'htmlOptions' => array('class' => 'form-horizontal', 'enctype'=>'multipart/form-data'),
 		'enableAjaxValidation'=>false,
-	)); ?>
+	));
+?>
 	<div class="col-xs-8 col-xs-offset-2">
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
 	<p id="to"></p>
 	<?php echo $form->errorSummary($model);
 	?>
+		<div class="row">
+			<?php echo $form->labelEx($model,'email'); ?>
+			<?php echo $form->textField($model,'email'); ?>
+			<?php echo $form->error($model,'email'); ?>
+		</div>
+		<?
+		$this->widget(
+			'bootstrap.widgets.TbCKEditor',
+			array(
+				'model' => $model,
+				'attribute' => 'body',
+				'editorOptions' => array(
+					// From basic `build-config.js` minus 'undo', 'clipboard' and 'about'
+					'plugins' => 'basicstyles,toolbar,enterkey,entities,floatingspace,wysiwygarea,indentlist,link,list,dialog,dialogui,button,indent,fakeobjects'
+				)
+			)
+		);
+		?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'body'); ?>
-		<?php echo $form->textField($model,'body'); ?>
-		<?php echo $form->error($model,'body'); ?>
-	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email'); ?>
-		<?php echo $form->error($model,'email'); ?>
-	</div>
 
 	<div class="row" style="visibility: hidden;">
 		<?php echo $form->labelEx($model,'parent_id'); ?>
 		<?php echo $form->textField($model,'parent_id'); ?>
 		<?php echo $form->error($model,'parent_id'); ?>
 	</div>
-
+		<div class="row buttons">
+			<?php echo CHtml::submitButton('Submit'); ?>
+		</div>
 	<div class="row" style="visibility: hidden;">
 		<?php echo $form->labelEx($model,'post_id'); ?>
 		<?php echo $form->textField($model,'post_id'); ?>
@@ -48,9 +55,7 @@
 
 
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Submit'); ?>
-	</div>
+
 
 	<?php $this->endWidget(); ?>
 		<a id="addComment"></a>
