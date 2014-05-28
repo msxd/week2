c954f55b5ed6097c345348a616709c4d2e2f1c55
msxd <valikov.ids@gmail.com>
2014-05-28 09:50:23 +0300

    gittest

--- js/app.js
+++ js/app.js
@@ -4,13 +4,12 @@
 
 function scroll_to_elem(elem, speed, pid) {
     $('#Comment_parent_id').val(pid);
-    $('#to').html('Comment #' + pid);
+    $('#to').html('Comment #' + pid).append('<span id="clear_parrent_comment"> X </span>');
     if (document.getElementById(elem)) {
         var destination = jQuery('#' + elem).offset().top;
         jQuery("html,body").animate({scrollTop: destination}, speed);
     }
 }
-
 function redir(){
     window.location.href = "/";
 }
@@ -22,3 +21,8 @@ function closeIt(elem) {
 
     $(elem).parent().fadeOut(1000).end().remove().done();
 }
+
+$('body').on('click','#clear_parrent_comment',function(){
+    $('#Comment_parent_id').val(null);
+    $('#to').html('post');
+});
